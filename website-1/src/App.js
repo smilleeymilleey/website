070163e505ee342b03be15a5ea8faae6d2f49345
unknown <>
2020-12-08 20:42:48 -0600

    created a path to pages in app js

--- website-1/src/App.js
+++ website-1/src/App.js
@@ -9,15 +9,17 @@ import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-  <>
-    <Router>
-    <Navbar />
-      <Switch>
-        <Route path="/" exact component = {Home} />
-      </Switch>
-    </Router>
-  </>
-
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
