c762fa6c8e770293e0acfaece622b6d63bf1f5b6
unknown <>
2020-12-10 15:05:00 -0600

    created a contact form, still need to backend sumbit, and created an about page

--- website-1/src/components/pages/Contact.js
+++ website-1/src/components/pages/Contact.js
@@ -1,6 +1,51 @@
 import React from 'react';
 import '../../App.css';
 
-export default function Contact() {
-  return <h1 className='contact'>Contact</h1>;
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Email Address:
+          <input
+            name="Email"
+            type="string"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Message:
+          <input
+            name="numberOfGuests"
+            type="string"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+        <button>Submit</button>
+      </form>
+    );
+  }
 }
+
+export default Contact;
--- website-1/src/components/pages/Services.js
+++ website-1/src/components/pages/Services.js
@@ -1,11 +1,27 @@
 
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
 
-export default function Services() {
-  return (
-    <h1 className='services'>About</h1>
+    function Services() {
+      return (
+        <>
+        <h1 className='services'>About</h1>
+          <HeroSection />
+          <Cards />
+          <Footer />
+        </>
+      );
 
-  )
+
+
+
+
+
+
 
 }
+
+export default Services;
