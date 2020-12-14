21209d481d9a3471d57dc80c042c79de12955dc9
unknown <>
2020-12-14 00:29:42 -0600

    fixed ABOUT ME section. its perfect

--- website-1/src/components/AboutHero.js
+++ website-1/src/components/AboutHero.js
@@ -8,27 +8,29 @@ import './AboutHero.css';
 function AboutHero() {
   return (
     <div className='hero-container'>
-    <h1 className="welcome"> About Me</h1>
+    <section>
+    <img className="meinside" src="meinside.jpg"></img>
+    </section>
 
-        <table className="table">
-          <tbody>
-            <tr>
-              <th className="header">
+    <h1 className="welcome"> About Me</h1>
+  
+
+
+              <h2 className="header">
                  My name is Caitlyn Miley and I am a Full Stack Developer!
-              </th>
-            </tr>
-            <tr>
-              <td>
+              </h2>
+
+
+
+              <section className="paragraph">
                    I have been programming for the past year and have worked on <br />
                  social media clones, e-commerce projects, and developing with API's.<br />
                  I have a Full Stack Certificate from Southern Methodist University<br />
                  and a Bachelors of Science and Engineering from Texas A&M-Commerce.
 
-             </td>
-           </tr>
+             </section>
 
-           </tbody>
-        </table>
+
 
 
 
