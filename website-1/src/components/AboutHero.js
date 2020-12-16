d6b110abb7b59c3d568d2f56ac10e5f28edf73e8
unknown <caitlynmiley.cm@gmail.com>
2020-12-16 03:15:17 -0600

    good responsive layout to play with

--- website-1/src/components/AboutHero.js
+++ website-1/src/components/AboutHero.js
@@ -5,36 +5,32 @@ import './AboutHero.css';
 
 
 
-function AboutHero() {
+
+export default function Blog() {
   return (
+
     <div className='hero-container'>
+        <h1 className="title">About Me</h1>
 
-      <section className = "Welcome1">
-          <h1 className="AboutMe"> About Me</h1>
-            <h2 className="Welcome">
-              Welcome! My name is Caitlyn. This is my story.
-            </h2>
+      <div class="wrapper">
 
+
+
+        <div class="Picture">
         <img alt="me" className="meinside" src="meinside1.jpg"></img>
+        </div>
 
-        <p className="paragraph">
-           I have been programming since 2019 and have worked on <br />
-              social media clones, e-commerce projects, and developing with API's.<br />
-                 I have a Full Stack Website Development Certificate from Southern Methodist University<br />
-                  and a Bachelors of Science and Engineering from Texas A&M-Commerce. My passion for programming <br /> began when
-                  I first learned that I could create something out of nothing.</p>
+        <div class="Paragraph">
+        <h2> This is my story </h2>
+        <p className="Paragraph">I have been programming since 2019 and have worked on <br />
+            social media clones, e-commerce projects, and developing with API's.<br />
+               I have a Full Stack Website Development Certificate from Southern Methodist University<br />
+                and a Bachelors of Science and Engineering from Texas A&M-Commerce. My passion for programming <br /> began when
+                I first learned that I could create something out of nothing.
+                </p>
+        </div>
 
-
-
-  </section>
-
-
-
-
-
-
-  </div>
-  );
+      </div>
+      </div>
+)
 }
-
-export default AboutHero;
