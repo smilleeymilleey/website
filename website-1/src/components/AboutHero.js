77b6ef6b5ac203559dff8893e7d128c38a9f31a3
unknown <caitlynmiley.cm@gmail.com>
2020-12-16 02:24:22 -0600

    saving a copy

--- website-1/src/components/AboutHero.js
+++ website-1/src/components/AboutHero.js
@@ -8,25 +8,25 @@ import './AboutHero.css';
 function AboutHero() {
   return (
     <div className='hero-container'>
-      <section>
-        <img className="meinside" alt="me" src="meinside1.jpg"></img>
-      </section>
 
-        <h1 className="welcome"> About Me</h1>
-            <h2 className="header">
+      <section className = "Welcome1">
+          <h1 className="AboutMe"> About Me</h1>
+            <h2 className="Welcome">
               Welcome! My name is Caitlyn. This is my story.
             </h2>
 
-        <section className="paragraph">
+        <img alt="me" className="meinside" src="meinside1.jpg"></img>
+
+        <p className="paragraph">
            I have been programming since 2019 and have worked on <br />
               social media clones, e-commerce projects, and developing with API's.<br />
                  I have a Full Stack Website Development Certificate from Southern Methodist University<br />
                   and a Bachelors of Science and Engineering from Texas A&M-Commerce. My passion for programming <br /> began when
-                  I first learned that I could create something out of nothing.
-
-        </section>
+                  I first learned that I could create something out of nothing.</p>
 
 
+
+  </section>
 
 
 
