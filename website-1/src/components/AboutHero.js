964bdc4cb8fde15efb796d191c96dd9c70650e8a
unknown <caitlynmiley.cm@gmail.com>
2020-12-16 13:30:43 -0600

    made image responsive

--- website-1/src/components/AboutHero.js
+++ website-1/src/components/AboutHero.js
@@ -4,8 +4,6 @@ import './AboutHero.css';
 
 
 
-
-
 export default function Blog() {
   return (
 
@@ -22,10 +20,10 @@ export default function Blog() {
 
         <div class="Paragraph">
         <h2> This is my story </h2>
-        <p className="Paragraph">I have been programming since 2019 and have worked on <br />
-            social media clones, e-commerce projects, and developing with API's.<br />
-               I have a Full Stack Website Development Certificate from Southern Methodist University<br />
-                and a Bachelors of Science and Engineering from Texas A&M-Commerce. My passion for programming <br /> began when
+        <p className="Paragraph">I have been programming since 2019 and have worked on 
+            social media clones, e-commerce projects, and developing with API's.
+               I have a Full Stack Website Development Certificate from Southern Methodist University
+                and a Bachelors of Science and Engineering from Texas A&M-Commerce. My passion for programming began when
                 I first learned that I could create something out of nothing.
                 </p>
         </div>
