b351e20b08f5f5dfaab02f0174fc042a12cb4fdd
unknown <caitlynmiley.cm@gmail.com>
2020-12-17 14:01:48 -0600

    added the backend to the contact me form

--- website-1/src/components/AboutHero.js
+++ website-1/src/components/AboutHero.js
@@ -10,10 +10,9 @@ function AboutHero() {
       <img src='aboutmeheader.png'></img>
       <h1 className="Title">About Me </h1>
       
-      <p className="Sub">Hey there! Thanks for cheecking out my portfolio.
-      I've been programming since 2019. I started with frontend languages like Javascript, HTML, and CSS. I eventually learned backend capabilities like 
-      React and Firebase, to my toolkit. I love programming because creating 
-      beautiful and functional programs from scratch feels like magic. 
+      <p className="Sub">Hey there! Thank you for visiting my portfolio.
+      I've been programming since 2019. I started with Frontend languages like Javascript, HTML, and CSS. I eventually moved on to obtain a Full Stack Web Development Certification from Southern Methodist University where I added React.Js, Firebase, 
+      and mySQL to my toolkit. I am passionate about programming because it allows me to merge both my analytical and creative skills into one profession. 
        </p>
       
         
--- website-1/src/components/Contact.js
+++ website-1/src/components/Contact.js
@@ -3,29 +3,63 @@ import "./Contact.css";
 
 
 
-export default function Contact() {
+export default class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
+
+  render() {
+    const { status } = this.state;
     return (
-  <div className="newContainer">
+      <form
+        onSubmit={this.submitForm}
+        action="https://formspree.io/f/xoqpkozl"
+        method="POST"
+      >
+        <div className="newContainer">
 <div class="wrapper">
   <div class="title">
     <h1>Contact Me</h1>
   </div>
-  <div class="contact-form">
-    <div class="input-fields">
-      <input type="text" class="input" placeholder="Name"></input>
-      <input type="text" class="input" placeholder="Email Address"></input>
-      <input type="text" class="input" placeholder="Phone"></input>
-      <input type="text" class="input" placeholder="Subject"></input>
-    </div>
-    <div class="msg">
-      <textarea placeholder="Message"></textarea>
-      <div class="btn">send</div>
-    </div>
-  </div>
-</div>
-</div>
+  <div className="contact-form">
+    <div className="input-fields">
 
+    <label className="EmailText">Email:</label>
+        <input className="emailBox" type="email" name="email" />
+        <label className="Message">Message:</label>
+        <input className="messageBox" type="text" name="message" />
+        {status === "SUCCESS" ? <p>Thanks!</p> : <button className="btn">Submit</button>}
+        {status === "ERROR" && <p>Ooops! There was an error.</p>}
 
+        </div>
+      </div>
+    </div>
+  </div> 
+</form>
     );
-};
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
 
