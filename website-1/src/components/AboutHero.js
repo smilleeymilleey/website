ee8e82ef678e6be748cbb452344c03d9882fe31f
unknown <caitlynmiley.cm@gmail.com>
2020-12-18 20:38:46 -0600

    added a responsive about me page

--- website-1/src/components/AboutHero.js
+++ website-1/src/components/AboutHero.js
@@ -15,9 +15,14 @@ function AboutHero() {
       <div className='abouthero-container'>
     
       <img className="aboutVideo" src='aboutmeheader.png'></img>
+       
+          <p className='aboutme-paragraph'> About Me. <br></br>
+             I have been programming since 2019. I enjoy using Javascript, 
+             React, Firebase, Node, HTML, and CSS.
+          
+          </p>
       
-      
-        
+             <button className= "resume">Resume</button>
      
         
 
