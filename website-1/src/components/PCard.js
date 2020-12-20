3bf36dd9e62e184aef7442f629dba3a45fd1b239
unknown <caitlynmiley.cm@gmail.com>
2020-12-20 15:07:06 -0600

    I updated cards so blog page and project page look different

--- website-1/src/components/PCard.js
+++ website-1/src/components/PCard.js
@@ -4,46 +4,46 @@ import './PCard.css';
 function PCard() {
     return (
 <div className="container">
-<section class="info">
+<section className="info">
   
   <h1>Learn HTML &mdash; <a href="https://codetheweb.blog/" target="_blank">Code The Web</a></h1>
 </section>
-<section class="cards-wrapper">
-  <div class="card-grid-space">
-    <div class="num">01</div>
-    <a class="card" href="https://codetheweb.blog/2017/10/06/html-syntax/">
+<section className="cards-wrapper">
+  <div className="card-grid-space">
+    <div className="num">01</div>
+    <a className="pCard" href="https://codetheweb.blog/2017/10/06/html-syntax/">
       <div>
         <h1>HTML Syntax</h1>
         <p>The syntax of a language is how it works. How to actually write it. Learn HTML syntax…</p>
-        <div class="date">6 Oct 2017</div>
-        <div class="tags">
-          <div class="tag">HTML</div>
+        <div className="date">6 Oct 2017</div>
+        <div className="tags">
+          <div className="tag">HTML</div>
         </div>
       </div>
     </a>
   </div>
-  <div class="card-grid-space">
-    <div class="num">02</div>
-    <a class="card" href="https://codetheweb.blog/2017/10/09/basic-types-of-html-tags/"> 
+  <div className="card-grid-space">
+    <div className="num">02</div>
+    <a className="pCard" href="https://codetheweb.blog/2017/10/09/basic-types-of-html-tags/"> 
       <div>
         <h1>Basic types of HTML tags</h1>
         <p>Learn about some of the most common HTML tags…</p>
-        <div class="date">9 Oct 2017</div>
-        <div class="tags">
-          <div class="tag">HTML</div>
+        <div className="date">9 Oct 2017</div>
+        <div className="tags">
+          <div className="tag">HTML</div>
         </div>
       </div>
     </a>
   </div>
-  <div class="card-grid-space">
-    <div class="num">03</div>
-    <a class="card" href="https://codetheweb.blog/2017/10/14/links-images-about-file-paths/"> 
+  <div className="card-grid-space">
+    <div className="num">03</div>
+    <a className="pCard" href="https://codetheweb.blog/2017/10/14/links-images-about-file-paths/"> 
       <div>
         <h1>Links, images and about file paths</h1>
         <p>Learn how to use links and images along with file paths…</p>
-        <div class="date">14 Oct 2017</div>
-        <div class="tags">
-          <div class="tag">HTML</div>
+        <div className="date">14 Oct 2017</div>
+        <div className="tags">
+          <div className="tag">HTML</div>
         </div>
       </div>
     </a>
