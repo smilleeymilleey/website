1a95d035ea756efc3892eaa5145a38c53f84174b
unknown <caitlynmiley.cm@gmail.com>
2020-12-19 20:44:10 -0600

    took out projects. starting over.

--- website-1/src/components/pages/Products.js
+++ website-1/src/components/pages/Products.js
@@ -3,15 +3,15 @@ import React from 'react';
 import '../../App.css'
 import Footer from '../Footer';
 import { Flex } from '../Flex';
-import ProjCard from '../ProjCard';
+;
 
 
 
 const Projects = ( ) => {
   return (
     <div>
-     <ProjCard />
-     <Footer />
+
+    
     </div>
   );
 };
