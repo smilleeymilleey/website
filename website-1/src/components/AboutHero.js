6cc3b1f5bc2aa301d4a9324240903d09ff2e3cd7
unknown <caitlynmiley.cm@gmail.com>
2020-12-21 05:23:15 -0600

    added buttons to the home page

--- website-1/src/components/AboutHero.js
+++ website-1/src/components/AboutHero.js
@@ -10,11 +10,13 @@ function AboutHero() {
 
   <div class="content">
   <div class="aboutcard">
-    <div class="firstinfo"><img src="https://randomuser.me/api/portraits/lego/6.jpg"/>
+    <div class="firstinfo"><img src="meinside1.jpg"/>
       <div class="profileinfo">
         <h1>Caitlyn Miley</h1>
         <h3>Web Devloper</h3>
         <p class="bio"> I am proficient in Javascript, React, HTML, CSS. I hold a Bachelors Of Science & Engineering and love bringing concepts to life through code!</p>
+        <button className="resumeButton">Resume</button>
+        <button className="letterButton">Letter Of Recomendation</button>
       </div>
     </div>
   </div>
